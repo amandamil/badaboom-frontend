088e2a48626b872e09a42e22307bbb70483cb6d7
Jesus Santos <jesustotos1121@gmail.com>
2020-06-29 23:14:26 +0200

    Background video transition issue

--- components/sections/FirstSection.vue
+++ components/sections/FirstSection.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="section section--first">
-    <video v-if="isMobile" class="section__background" data-autoplay muted loop>
-      <source src="~assets/videos/background-mobile.mp4" type="video/mp4" />
-    </video>
-    <video v-else loop muted data-autoplay class="section__background">
-      <source src="~assets/videos/background-desktop.mp4" type="video/mp4" />
-    </video>
+  <div :class="['section section--first', { active: activeSection }]">
     <div class="container container--text-center">
+      <video
+        v-if="isMobile"
+        class="section__background mobile"
+        data-autoplay
+        muted
+        loop
+        playsinline
+      >
+        <source src="~assets/videos/background-mobile.mp4" type="video/mp4" />
+      </video>
+      <video
+        v-else
+        loop
+        muted
+        data-autoplay
+        playsinline
+        class="section__background"
+      >
+        <source src="~assets/videos/background-desktop.mp4" type="video/mp4" />
+      </video>
       <h1
         v-animate="animationOption"
         class="section-title section-title--first"
@@ -26,6 +40,7 @@ export default {
   name: 'FirstSection',
   data() {
     return {
+      active: 1,
       animationOption: {
         stagger: [
           {
@@ -45,6 +60,20 @@ export default {
       return !!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
         navigator.userAgent
       )
+    },
+    activeSection() {
+      return this.actice === 1
+    }
+  },
+  created() {
+    this.$root.$on('active-section', this.activeSectionHandler)
+  },
+  beforeDestroy() {
+    this.$root.$off('active-section', this.activeSectionHandler)
+  },
+  methods: {
+    activeSectionHandler(value) {
+      this.active = value
     }
   }
 }
